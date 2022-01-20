a69519e139ef035d215c91c80a82dd8f5d79bd9d
Svetlana Prus <swetlana.prus@gmail.com>
2022-01-20 19:54:42 +0100

    kata-6: zipWith, ver.2

--- kata-6kyu/zipWith.js
+++ kata-6kyu/zipWith.js
@@ -2,7 +2,6 @@
 // The function value is one new array. If the arrays are of unequal length, the output will only be as long as the shorter one.
 // (Values of the longer array are simply not used.) Inputs should not be modified.
 
-
 function zipWith(fn, a0, a1) {    // Input: Math.pow, [10,10,10,10], [0,1,2,3]
     const shortestArray = a0.length < a1.length ? a0 : a1;
     let result = [];
@@ -13,3 +12,29 @@ function zipWith(fn, a0, a1) {    // Input: Math.pow, [10,10,10,10], [0,1,2,3]
 
     return result;
 }
+
+// Version 2 -
+// function zipWith(fn,a0,a1) {
+//     const symbols = {
+//         plus: '+',
+//         minus: '-',
+//         multiply: '*',
+//         divide: '/',
+//     }
+//
+//     let result = [];
+//     const minArray = Math.min(a0.length, a1.length);
+//
+//     for(let i = 0; i < minArray; i++){
+//         if(fn in symbols){
+//
+//             let f2 = new Function(`return ${a0[i] + symbols.fn + a1[i]}`);
+//             result.push(f2());
+//
+//         } else {
+//             result.push(fn(a0[i], a1[i]));
+//         }
+//     }
+//     return result;
+// }
+
