b5a4f397f22826738b67dd93eb76af96a9b4aec1
Svetlana Prus <swetlana.prus@gmail.com>
2022-01-20 20:21:05 +0100

    kata-7: Is every value in the array an array?, ver.2-old

--- kata-7kyu/isEveryValueInTheArrayAnArray.js
+++ kata-7kyu/isEveryValueInTheArrayAnArray.js
@@ -1,3 +1,14 @@
 function arrCheck(arr){
     return arr.every(el => Array.isArray(el));
 }
+
+
+// Old version -
+
+// function arrCheck(value){
+//     let count = 0;
+//     for (let el of value) {
+//         if (Array.isArray(el) === true) count += 1;
+//     }
+//     return (count === value.length)? true : false;
+// }
