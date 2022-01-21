bdc33b06030be381dd2bbaa4df6db41df81228e9
Svetlana Prus <swetlana.prus@gmail.com>
2022-01-21 22:42:11 +0100

    kata-6: Data Reverse

---
+++ kata-6kyu/dataReverse.js
@@ -0,0 +1,21 @@
+//Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+// 11111111  00000000  00001111  10101010
+//
+// should become:
+// 10101010  00001111  00000000  11111111
+
+// The total number of bits will always be a multiple of 8.
+// The data is given in an array as such: [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+function dataReverse(data) {
+    const array = [];
+
+    for (let i = 0; i < data.length; i = i + 8) {
+        array.push(data.slice(i, i + 8));
+    }
+
+    array.reverse();
+    // return array.flat();   // It doesn't work
+
+    return [].concat(...array);
+}
