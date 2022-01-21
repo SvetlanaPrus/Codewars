ce5fa69b80bd0343187f631c8fcce839184b005c
Svetlana Prus <swetlana.prus@gmail.com>
2022-01-21 17:25:26 +0100

    kata-6: Two Sum, refactoring

--- kata-6kyu/twoSum.js
+++ kata-6kyu/twoSum.js
@@ -1,14 +1,27 @@
 // Write a function that takes an array of numbers (integers for the tests) and a target number.
 // It should find two different items in the array that, when added together, give the target value.
 
-function twoSum(arr, num) {
-    let sum = 0;
+// Refactoring - (clean "sum")
 
+function twoSum(arr, num) {
     for (let a = 0; a < arr.length - 1; a++) {
         for (let b = a + 1; b < arr.length; b++) {
-            sum = arr[a] + arr[b];
 
-            if (sum === num) return [a, b];
+            if (arr[a] + arr[b] === num) return [a, b];
         }
     }
 }
+
+// function twoSum(arr, num) {
+//     let sum = 0;
+//
+//     for (let a = 0; a < arr.length - 1; a++) {
+//         for (let b = a + 1; b < arr.length; b++) {
+//             sum = arr[a] + arr[b];
+//
+//             if (sum === num) return [a, b];
+//         }
+//     }
+// }
+
+
