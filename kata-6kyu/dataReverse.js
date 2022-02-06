be3c5fbcd67ab524ee96f17eab0a84ab84c88a7d
Svetlana Prus <swetlana.prus@gmail.com>
2022-02-06 14:51:06 +0100

    6 kata - Data Reverse, refactoring

--- kata-6kyu/dataReverse.js
+++ kata-6kyu/dataReverse.js
@@ -7,15 +7,28 @@
 // The total number of bits will always be a multiple of 8.
 // The data is given in an array as such: [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
 
+
+// Refactoring -
 function dataReverse(data) {
     const array = [];
 
-    for (let i = 0; i < data.length; i = i + 8) {
-        array.push(data.slice(i, i + 8));
+    for (let i = 0; i < data.length; i += 8) {
+        array.unshift(...data.slice(i, i + 8));
     }
 
-    array.reverse();
-    // return array.flat();   // It doesn't work
-
-    return [].concat(...array);
+    return array;
 }
+
+
+// function dataReverse(data) {
+//     const array = [];
+//
+//     for (let i = 0; i < data.length; i = i + 8) {
+//         array.push(data.slice(i, i + 8));
+//     }
+//
+//     array.reverse();
+//     // return array.flat();   // It doesn't work
+//
+//     return [].concat(...array);
+// }
