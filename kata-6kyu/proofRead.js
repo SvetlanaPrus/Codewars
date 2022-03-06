1f289f8a5216be5897c6517f0d6a30e2ed2cef45
Svetlana Prus <swetlana.prus@gmail.com>
2022-03-06 14:03:27 +0100

    6 Kata - Proof Read

---
+++ kata-6kyu/proofRead.js
@@ -0,0 +1,17 @@
+// 6 Kata - Proof Read
+
+//You've just finished writing the last chapter for your novel when a virus suddenly infects your document.
+//It has swapped the 'i's and 'e's in 'ei' words and capitalised random letters. Write a function which will:
+//a) remove the spelling errors in 'ei' words. (Example of 'ei' words: their, caffeine, deceive, weight)
+//b) only capitalise the first letter of each sentence. Make sure the rest of the sentence is in lower case.
+
+//Example: "He haD iEght ShOTs of CAffIEne." --> "He had eight shots of caffeine."
+
+function proofread (str) {
+    return str
+        .toLowerCase()
+        .replaceAll('ie', 'ei')
+        .split('. ')
+        .map(str => str[0].toUpperCase() + str.slice(1))
+        .join('. ');
+}
